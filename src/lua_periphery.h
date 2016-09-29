2275d451db5f329b0114e0967fc8c586f96f743f
Vanya A. Sergeev <v@sergeev.io>
2016-09-29 04:43:03 -0700

    update version to v1.1.0

--- src/lua_periphery.h
+++ src/lua_periphery.h
@@ -7,12 +7,12 @@
 #ifndef _LUA_PERIPHERY_H
 #define _LUA_PERIPHERY_H
 
-#define LUA_PERIPHERY_VERSION           "1.0.6"
-#define LUA_PERIPHERY_MMIO_VERSION      "1.0.6"
-#define LUA_PERIPHERY_GPIO_VERSION      "1.0.6"
-#define LUA_PERIPHERY_SPI_VERSION       "1.0.6"
-#define LUA_PERIPHERY_I2C_VERSION       "1.0.6"
-#define LUA_PERIPHERY_SERIAL_VERSION    "1.0.6"
+#define LUA_PERIPHERY_VERSION           "1.1.0"
+#define LUA_PERIPHERY_MMIO_VERSION      "1.1.0"
+#define LUA_PERIPHERY_GPIO_VERSION      "1.1.0"
+#define LUA_PERIPHERY_SPI_VERSION       "1.1.0"
+#define LUA_PERIPHERY_I2C_VERSION       "1.1.0"
+#define LUA_PERIPHERY_SERIAL_VERSION    "1.1.0"
 
 #endif
 
